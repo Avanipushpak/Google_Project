21fc1d59d03611f24b112bb8401f25ae5d1aac1f
Avani <Avani@LAPTOP-4M94SFNL>
2018-04-08 11:08:21 -0500

    Changes done

--- Google_Project/src/test/java/com/Facebook/login/Facebook_Logn.java
+++ Google_Project/src/test/java/com/Facebook/login/Facebook_Logn.java
@@ -3,29 +3,29 @@ package com.Facebook.login;
 import org.testng.annotations.Test;
 
 public class Facebook_Logn {
-	@Test(priority=1,enabled=false)
+	@Test(priority = 1, enabled = false)
 	public void Login() {
-		System.out.println("This is Login");
+		System.out.println("This is Login Script");
 	}
-	
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void Account() {
-		System.out.println("Account");
+		System.out.println("Create Account");
 	}
-	
-	@Test(priority=3)
+
+	@Test(priority = 3)
 	public void searchAccount() {
 		System.out.println("Search Account");
 	}
-	
-	@Test(priority=4)
+
+	@Test(priority = 4)
 	public void CreateNewAccount() {
 		System.out.println("Created new Account");
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
