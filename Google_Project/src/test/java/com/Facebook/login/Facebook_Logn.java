92f8f58f931f37794cf6b136a3301aca2ec11ff3
Avani <Avani@LAPTOP-4M94SFNL>
2018-04-08 10:54:01 -0500

    sfbs

---
+++ Google_Project/src/test/java/com/Facebook/login/Facebook_Logn.java
@@ -0,0 +1,20 @@
+package com.Facebook.login;
+
+import org.testng.annotations.Test;
+
+public class Facebook_Logn {
+	@Test(priority=1,enabled=false)
+	public void Login() {
+		System.out.println("This is Login");
+	}
+	
+	@Test(priority=2)
+	public void Account() {
+		System.out.println("Account");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
